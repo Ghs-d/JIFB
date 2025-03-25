3b1d2c743d898bab706883fee63c2c17de2c98d2
Silas <silasandrade.code@gmail.com>
2025-03-25 11:22:56 -0300

    main

--- jornalista/urls.py
+++ jornalista/urls.py
@@ -7,8 +7,5 @@ urlpatterns = [
     path("news/", views.news_view, name = 'news'),
     path("editar/<int:id>", views.editar_news, name = 'editar'),
     path("remover/<int:id>", views.remover_news, name = 'remover'),
-    path('quemsomos/', views.quem_somos, name='quem_somos'),
-
-   
-
+    path('quemsomos/', views.quem_somos, name='quem_somos'), 
 ]
