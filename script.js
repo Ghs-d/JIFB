a1c565c5da3f29eb77c824626b95ce163ba380ee
Silas <silasandrade.code@gmail.com>
2025-04-02 17:35:01 -0300

    ♻️ refactor: Criei nova Branch para mim

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const errorMessage = document.getElementById("errorMessage");
+  
+ 
+});
+
+
