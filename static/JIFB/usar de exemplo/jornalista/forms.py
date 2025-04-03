d191e3516cf62577ba9f56a90a1204564ab0d410
Silas <silasandrade.code@gmail.com>
2025-04-02 22:21:17 -0300

    Merge 'Silas' e 'Main'

--- static/JIFB/usar de exemplo/jornalista/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import NotíciaModel
-
-
-class NoticiasForm(forms.ModelForm):
-    class Meta:
-        model = NotíciaModel
-        fields = ['tituto', 'noticia', 'autor']
